7f70468ad2414e1f209e8c3f759ac4bc28bcaf90
divisen rungadoo <divisen@engineer.com>
2022-05-21 21:00:26 +0400

    Activity Model Created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_21_162450) do
+ActiveRecord::Schema.define(version: 2022_05_21_165602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "activities", force: :cascade do |t|
+    t.text "details"
+    t.boolean "is_available"
+    t.integer "price"
+    t.string "address"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_activities_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +39,5 @@ ActiveRecord::Schema.define(version: 2022_05_21_162450) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "activities", "users"
 end
