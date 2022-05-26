be798c9dd9ed84a06002b6cb5eb4726fa41352d5
Nirmaan Sonatun <nsonatun@hotmail.com>
2022-05-26 17:18:20 +0400

    ReviewsController done and routing done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,24 @@
 class ReviewsController < ApplicationController
+
+  def create
+    @review = Review.new(review_params)
+    @activity = Activity.find(params[:activity_id])
+    @review.activity = @activity
+    if @review.save
+      redirect_to activity_path(@activity)
+    else
+      render 'activities/show'
+  end
+
+  def destroy
+    @review =review.find(params[:id])
+    @review.destroy
+    redirect_to activity_path(@review.activity)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
+  end
 end
