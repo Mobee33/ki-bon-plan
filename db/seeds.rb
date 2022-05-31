5006c9cce3fa0bced9c9ca1ed3ba157071624559
Mubeen Bankur <mobee33@hotmail.com>
2022-05-31 16:01:57 +0400

    seeds file refactoring

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,8 @@ photo = activities["entries"][i]["photo"]
 file = URI.open("#{photo}")
 Activity.create!(title: "#{activities["entries"][i]["title"]}", details: "#{activities["entries"][i]["details"]}", address: "#{activities["entries"][i]["address"]}", price: "#{activities["entries"][i]["price"]}", user_id: "#{user1.id}").photo.attach(io: file, filename: 'activity.jpg', content_type: 'image/jpg')
 
-5.times do
+count = "#{activities["entries"].count - 1}".to_i
+count.times do
   i += 1
   user1 = User.create(email: "#{users["entries"][i]["email"]}", password: "#{users["entries"][i]["password"]}")
   photo = activities["entries"][i]["photo"]
